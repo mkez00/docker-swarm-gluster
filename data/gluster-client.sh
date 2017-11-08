c77c27f547f09e6995af3be63aa11e65fb70df40
Michael Kezele <michael.kezele@scenedoc.com>
2017-11-08 08:43:28 -0500

    gluster client config should mount gluster on system boot

--- data/gluster-client.sh
+++ data/gluster-client.sh
@@ -3,3 +3,4 @@
 # configure gluster mount
 mount -t glusterfs 192.168.56.81:/gvol0 /mnt/glusterfs
 mkdir -p /mnt/glusterfs/var/lib/grafana
+echo "192.168.56.81:/gvol0 /mnt/glusterfs glusterfs  defaults,_netdev 0 0" >> /etc/fstab
