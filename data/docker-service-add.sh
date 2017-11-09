3da8abf7e55630b451f790ad5c62d38912356314
Michael Kezele <michael.kezele@scenedoc.com>
2017-11-09 11:26:07 -0500

    cleaner way to create docker service

--- data/docker-service-add.sh
+++ data/docker-service-add.sh
@@ -3,10 +3,8 @@
 
 # Install Grafana
 docker pull grafana/grafana
-ID="$(docker service create --replicas 1 --name grafana --mount type=bind,source=/mnt/glusterfs/var/lib/grafana,target=/var/lib/grafana grafana/grafana)"
-docker service update --publish-add 3000:3000 $ID
+docker service create --replicas 1 --name grafana --mount type=bind,source=/mnt/glusterfs/var/lib/grafana,target=/var/lib/grafana --publish 3000:3000/tcp grafana/grafana
 
 # Install Prometheus
 docker pull prom/prometheus
-ID="$(docker service create --replicas 1 --name prometheus prom/prometheus)"
-docker service update --publish-add 9090:9090 $ID
+docker service create --replicas 1 --name prometheus --publish 9090:9090/tcp prom/prometheus
